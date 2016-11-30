212ee3659198369730e0b6691dd36775c1dd097f
finley <finley.lu@essist.com.tw>
2016-12-01 00:22:21 +0800

    modified:   index.js 	modified:   package.json 	modified:   public/hello.html 	new file:   public/js/socket.io.js

--- server/index.js
+++ server/index.js
@@ -1,36 +1,23 @@
 var koa = require('koa');
-var mount =require('koa-mount');
 var router = require('koa-router');
-var logger = require('koa-logger')
-var render = require('koa-swig');
+var logger = require('koa-logger');
+var serve = require('koa-static');
 var app = koa();
- 
-app.context.render = render({
-	root: './public/',
-	autoescape: true,
-	cache: false,
-	ext: 'html'
-});
 
 //使用logger日志库
 app.use(logger());
- 
-//首页校验函数
-var validate = function *(next){
-	console.log('validate');
-	console.log(this.request);
-    yield next;
-};
- 
-// 首页处理函数
-var index = function *(){
-    yield* this.render('hello');
-};
- 
-//路由处理，首页指定用index函数处理，但需要先经过validate函数校验
-var APIv1 = new router();
-APIv1.get('', validate, index);
-app.use(mount('/', APIv1.middleware()));
- 
+
+app.use(serve('./public'));
+
+var server = require('http').Server(app.callback()),
+    io = require('socket.io').listen(server);
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
+    });
+});
+
 // 监听3000端口
-app.listen(3000);
+server.listen(3000);
